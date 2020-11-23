12acb27a0bcc6a8b2ba29bb2e9c0a5e76c51e277
Faiyaz Haider <faiyaz7283@gmail.com>
2020-11-23 00:05:12 -0500

    Fixed k8s cluster join script

--- k8s_join_cluster.sh
+++ k8s_join_cluster.sh
@@ -5,37 +5,13 @@ set -euo pipefail
 # Variables
 #-------------------------------------------------------------------------------
 
-master_host="${1:-192.168.1.175:6443}"
-token="$(kubeadm token generate)"
-ca_cert_hash="$(openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | \
-		 openssl rsa -pubin -outform der 2>/dev/null | \
-		 openssl dgst -sha256 -hex | sed 's/^.* //')"
-
-
-echo $master_host
-exit
-
-#-------------------------------------------------------------------------------
-# Functions
-#-------------------------------------------------------------------------------
-
-set_token() {
-  kubeadm token create $token 
-}
-
-del_token() {
-  kubeadm token delete $token
-}
+token=${1}
+master_host=${2:-192.168.1.175:6443}
+ca_cert_hash=${3:-sha256:e6e5b73453f1ff5e1fc9654113e11c9aae70e097d2043828cc13c01f3ff8565f}
 
 #-------------------------------------------------------------------------------
 # Main
 #-------------------------------------------------------------------------------
 
-# Set the token for use
-set_token
-
 # Join the cluster
 kubeadm join $master_host --token $token --discovery-token-ca-cert-hash $ca_cert_hash
-
-# Cleanup the token
-del_token
